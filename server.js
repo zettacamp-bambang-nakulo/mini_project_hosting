82a401f7c7bab93aa33083f4cd657527fb31d2ab
zettacamp-bambang-nakulo <114063916+zettacamp-bambang-nakulo@users.noreply.github.com>
2022-12-02 10:29:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const loadUser= require("./userLoader")
 const trans_resolvers= require("./transactions/trans_resolvers")
 
 
-mongoose.connect("mongodb+srv://bambang:zettacamp@miniproject.nqkadwu.mongodb.net/restaurants?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://bambang:zettacamp@miniproject.nqkadwu.mongodb.net/restaurans?retryWrites=true&w=majority")
 const db = mongoose.connection 
 db.on('error', console.error.bind(console, 'connection error:')); 
 db.once('open', function() { 
