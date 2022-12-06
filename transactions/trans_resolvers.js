3da9e7a875bf099389bf904e24ebaa5680b2923e
zettacamp-bambang-nakulo <114063916+zettacamp-bambang-nakulo@users.noreply.github.com>
2022-12-06 11:33:48 +0700

    new update affter discount

--- transactions/trans_resolvers.js
+++ transactions/trans_resolvers.js
@@ -417,6 +417,25 @@ async function addCart(parent,{menu,order_date},context){
 
 }
 
+async function AfterDiscount(menu,args,context){
+    // console.log(menu.recipe_id)
+    let afterDiscount = 0
+    const recipe = await recipeModel.findById(menu.recipe_id).lean()
+    // console.log(recipe.special_offers)
+    if (recipe){
+        afterDiscount = (recipe.price *menu.amount)
+        // console.log(Total_Recipe)
+    }
+    if(recipe.special_offers === true){
+        currentPrice = recipe.price *menu.amount;
+        dicount = currentPrice *(recipe.discount/100)
+        // console.log(recipe.discount/100)
+        afterDiscount = (currentPrice-dicount)
+        // console.log(recipe.price)
+    }
+return afterDiscount
+}
+
 async function TotalRecipe(menu,args,context){
     // console.log(menu.recipe_id)
     let Total_Recipe = 0
@@ -619,6 +638,7 @@ const trans_resolvers={
     },
     trans_menu:{
         recipe_id:loadingredient,
+        affter_discount:AfterDiscount,
         total_recipe:TotalRecipe
     },
     transactions:{
