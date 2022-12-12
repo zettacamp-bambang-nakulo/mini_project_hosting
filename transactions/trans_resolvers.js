3552de07429e622e73e65f98ac3f8fc294bbd86c
zettacamp-bambang-nakulo <114063916+zettacamp-bambang-nakulo@users.noreply.github.com>
2022-12-12 09:59:53 +0700

    Add files via upload

--- transactions/trans_resolvers.js
+++ transactions/trans_resolvers.js
@@ -368,18 +368,20 @@ async function validateStockIngredient(user_id,id, menus){
     //      throw new ApolloError(" amount lebih dari avaible")
     //  }
     let total_all = await getTotal({menu:menus})
-    // console.log(t)
     if(user.saldo < total_all ){
         throw new ApolloError("less balance")
-    }
-    await userModel.updateOne({_id:user_id},
-        {
-            $set:{
-                saldo:user.saldo-=total_all
-            }
-        }
-        )    
+    } 
      const ValidasiSuccess = await transModel.findByIdAndUpdate(id,{menu:menus, total:total,order_status:"success"},{new:true})
+     if(ValidasiSuccess.order_status === "success"){
+        await userModel.updateOne({_id:user_id},
+            {
+                $set:{
+                    saldo:user.saldo-=total_all
+                }
+            }
+            )
+        
+     }
      reduceingredientStock(ingredientMap);
     //  userSaldo
     return ValidasiSuccess
